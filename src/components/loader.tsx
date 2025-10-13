cc6001f3543b9dd55c30536ddf57025cbb68cba7
23AI016 ELANGO K <23ai016@nandhaengg.org>
2025-10-13 05:22:24 +0000

    HTML <div class="loader-ball-1">     <div class="balls-1"></div>     <di

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-import { Code2 } from 'lucide-react';
-
 export function Loader() {
   return (
     <div className="fixed inset-0 z-[100] flex items-center justify-center bg-background">
-      <div className="loader">
-        <Code2 className="h-16 w-16 text-primary" />
+      <div className="loader-ball-1">
+        <div className="balls-1"></div>
+        <div className="balls-2"></div>
+        <div className="balls-3"></div>
+        <div className="balls-4"></div>
+        <div className="balls-5"></div>
+        <div className="balls-6"></div>
+        <div className="balls-7"></div>
+        <div className="balls-8"></div>
+        <div className="balls-9"></div>
       </div>
     </div>
   );
